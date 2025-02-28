6eb6461fa9c4356496927aed9a04ca6a7edba9aa
ikshaSinha <147754529+ikshaSinha@users.noreply.github.com>
2025-02-28 17:51:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (74.13%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,9 +2,14 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t) return true;
-        else return false;
-    }
+        vector<int> freq(26,0);
+        for(int i=0;i<s.size();i++){
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+        }
+        for(int cnt :freq){
+    if(cnt !=0) return false;
+        }  
+return true;
+  }
 };
